ce4e5470f28a91c17c598cb8ba7a2d4f5f19f7d6
JulianMusial <15865@ait.nsw.edu.au>
2025-11-26 06:23:16 +0000

    added some phps and github fldr

---
+++ fragments/footer.php
@@ -0,0 +1,22 @@
+    <footer class="main-footer">
+        <nav class="social">
+            <a href="https://facebook.com">
+                <i class="fa-brands fa-facebook"></i>
+            </a>
+            <a href="https://instagram.com">
+                <i class="fa-brands fa-square-instagram"></i>
+            </a>
+            <a href="https://x.com">
+                <i class="fa-brands fa-square-x-twitter"></i>
+            </a>
+            <a href="https://linkedin.com">
+                <i class="fa-brands fa-square-linkedin"></i>
+            </a>
+        </nav>
+        <nav class="footer-nav">
+            <a href="contact.html">Contact</a>
+            <a href="#">Terms</a>
+            <a href="#">Support</a>
+            <a href="#">Policies</a>
+        </nav>
+    </footer>
